4460ae349be66dad65e9a9c348226eb36a35d6bb
lby <2283165894@qq.com>
2023-10-10 15:10:36 +0800

    鉴权

--- chatgpt-data-trigger/src/main/java/com/lby/chatgpt/data/trigger/http/ChatGPTAIServiceController.java
+++ chatgpt-data-trigger/src/main/java/com/lby/chatgpt/data/trigger/http/ChatGPTAIServiceController.java
@@ -1,10 +1,12 @@
 package com.lby.chatgpt.data.trigger.http;
 
 import com.alibaba.fastjson.JSON;
+import com.lby.chatgpt.data.domain.auth.service.IAuthService;
 import com.lby.chatgpt.data.domain.openai.model.aggregates.ChatProcessAggregate;
 import com.lby.chatgpt.data.domain.openai.model.entity.MessageEntity;
 import com.lby.chatgpt.data.domain.openai.service.IChatService;
 import com.lby.chatgpt.data.trigger.http.dto.ChatGPTRequestDTO;
+import com.lby.chatgpt.data.types.common.Constants;
 import com.lby.chatgpt.data.types.exception.ChatGPTException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.stream.Collectors;
 
 /**
@@ -20,11 +23,14 @@ import java.util.stream.Collectors;
 @Slf4j
 @RestController()
 @CrossOrigin("${app.config.cross-origin}")
-@RequestMapping("/api/${app.config.api-version}/")
+@RequestMapping("/api/${app.config.api-version}/chatgpt/")
 public class ChatGPTAIServiceController {
 
     @Resource
     private IChatService chatService;
+
+    @Resource
+    private IAuthService authService;
 
     /**
      * 流式问题，ChatGPT 请求接口
@@ -52,7 +58,21 @@ public class ChatGPTAIServiceController {
             response.setCharacterEncoding("UTF-8");
             response.setHeader("Cache-Control", "no-cache");
 
-            // 2. 构建参数
+            // 2. 构建异步响应对象【对 Token 过期拦截】
+            ResponseBodyEmitter emitter = new ResponseBodyEmitter(3 * 60 * 1000L);
+            boolean success = authService.checkToken(token);
+
+            if (!success) {
+                try {
+                    emitter.send(Constants.ResponseCode.TOKEN_ERROR.getCode());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                emitter.complete();
+                return emitter;
+            }
+
+            // 3. 构建参数
             ChatProcessAggregate chatProcessAggregate = ChatProcessAggregate.builder()
                     .token(token)
                     .model(request.getModel())
@@ -65,7 +85,7 @@ public class ChatGPTAIServiceController {
                             .collect(Collectors.toList()))
                     .build();
 
-            // 3. 请求结果&返回
+            // 4. 请求结果&返回
             return chatService.completions(chatProcessAggregate);
         } catch (Exception e) {
             log.error("流式应答，请求模型：{} 发生异常", request.getModel(), e);
